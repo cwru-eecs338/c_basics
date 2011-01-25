a447d6dcb52832e4cc1988f7e0357c44f99f699d
Gary Doran, Jr <garydoranjr@gmail.com>
2011-01-24 20:42:45 -0500

    Add boolean examples

--- basics.c
+++ basics.c
@@ -1,16 +1,39 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<unistd.h>
 
+void booleans();
 void memory_management();
 char *stack_cat(const char*, const char*);
 char *heap_cat(const char*, const char*);
 
 int main() {
-
+    booleans();
     memory_management();
-
     return 0;
+}
+
+void booleans() {
+    // No boolean type
+    // 0 is false, any
+    // other number is true
+    int f = (1 == 0);
+    int t = (1 == 1);
+    printf("True: %d, False: %d\n", t, f);
+
+    // Useful for error handling, since
+    // 0 (false) usually means no errors
+    if(chdir("adirectorythatprobablydoesntexist")) {
+        perror("Error changing directory");
+    }
+
+    // Be Careful!
+    int i = 1;
+    int j = 2;
+    if( i = j ) {
+        puts("Is 1 equal to 2???");
+    }
 }
 
 void memory_management() {
